6b65137a0f4ae09ed2cb5564f398fcc95077c3c0
srikanthnani <srikanthnani1202@gmail.com>
2024-04-06 18:22:21 +0530

    (add: backend) user resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoreModule, CatsModule, AuthModule],
+  imports: [CoreModule, CatsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
